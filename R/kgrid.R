aae8361b8498d11d84dbea59e17538e78c412814
evantoth1 <evan.l.toth@lawrence.edu>
2026-05-21 08:08:03 +0000

    Beginning with Roxygen

--- R/kgrid.R
+++ R/kgrid.R
@@ -1,3 +1,13 @@
+#' Title
+#'
+#' @param min_k
+#' @param max_k
+#' @param step
+#'
+#' @returns
+#' @export
+#'
+#' @examples
 kgrid <- function(min_k, max_k, step) {
   expand.grid(k = seq(min_k, max_k, by = step))
 }
